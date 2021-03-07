b92fea5d7e9dbddb4071b462523e7f9249c5c59f
Howard Berg <hbergg@hotmail.com>
2021-03-07 16:46:18 -0500

    completed module

--- ssg.py
+++ ssg.py
@@ -1,5 +1,6 @@
 import typer
 import ssg.parsers 
+import sys
 
 
 from ssg.site import Site
@@ -7,6 +8,13 @@ from ssg.site import Site
 def main(source = "content",dest = "dist"):
     config = {'source': source, 'dest': dest, 'parsers': [ssg.parsers.ResourceParser()]}
     Site(**config).build()
+
+
+@staticmethod
+def error(message):
+    sys.stderr.write("\x1b[1;31m{}\n").format(message)
+
+
 
 
 #if __name__ == "__main__":
